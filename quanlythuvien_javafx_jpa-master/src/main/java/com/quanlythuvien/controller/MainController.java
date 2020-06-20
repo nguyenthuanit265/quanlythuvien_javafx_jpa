f7bf9f7602d4f08bb95b4afb0045807f09b91c55
hoangknguyen1807 <hoangknguyen.1807@gmail.com>
2020-06-20 16:58:02 +0700

    Some minor changes. Use built-in 'save' function instead of creating a new one

--- quanlythuvien_javafx_jpa-master/src/main/java/com/quanlythuvien/controller/MainController.java
+++ quanlythuvien_javafx_jpa-master/src/main/java/com/quanlythuvien/controller/MainController.java
@@ -5,32 +5,23 @@ import com.quanlythuvien.entity.Book;
 import com.quanlythuvien.entity.Employee;
 import com.quanlythuvien.repository.BookRepository;
 import com.quanlythuvien.repository.EmployeeRepository;
-import javafx.application.Platform;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.Window;
-import javafx.util.Callback;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Controller;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -52,8 +43,6 @@ public class MainController implements Initializable {
     @FXML
     private TextField txtfldSearchBook;
     @FXML
-    private Button btnEditBook;
-    @FXML
     private Button btnDeleteBook;
     @FXML
     private Button btnRefreshBooks;
@@ -72,10 +61,6 @@ public class MainController implements Initializable {
 
     public TextField getTxtfldSearchBook() {
         return txtfldSearchBook;
-    }
-
-    public Button getBtnEditBook() {
-        return btnEditBook;
     }
 
     public Button getBtnDeleteBook() {
@@ -98,9 +83,6 @@ public class MainController implements Initializable {
 
     private ConfigurableApplicationContext springContext;
 
-    @FXML
-    private StackPane stackPaneHolder;
-
     public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
@@ -117,15 +99,18 @@ public class MainController implements Initializable {
     public MainController() {
     }
 
-    // Is replaced with
-//    @PostConstruct
-//    void intialize() {
-//        System.out.println("bbbbbbb");
-//        List<Employee> list = employeeRepository.findAll();
-//        System.out.println("here employee controller");
-//        System.out.println(list);
-//    }
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+//        studentList = FXCollections.observableArrayList(
+//                new Student(1, "Chau", "chau@gmail.com", 21),
+//                new Student(2, "Chuong", "chuong@gmail.com", 20)
+//        );
+//        idColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("id"));
+//        nameColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
+//        emailColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
+//        ageColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("age"));
+//        table.setItems(studentList);
+    }
 
     public void populateTableViewBooks() {
         //txtfldSearchBook.setText(Double.toString(btnAddBook.getHeight()));
@@ -138,40 +123,6 @@ public class MainController implements Initializable {
 
         tableViewBooks.setItems(listBooks);
     }
-
-    /*public void addSomeBooksToTableView() {
-        List<Book> templist = new ArrayList<>();
-
-        Book newbook = new Book();
-        newbook.setName("Toi Tai Gioi");
-        newbook.setCategory("Self-Help");
-        newbook.setAuthor("somebody");
-        newbook.setPublisher("tre");
-        newbook.setYearPublished("2010");
-        templist.add(newbook);
-
-        newbook = new Book();
-        newbook.setName("Cafe voi Tony");
-        newbook.setCategory("Self-Help");
-        newbook.setAuthor("Tony BS");
-        newbook.setPublisher("tre");
-        newbook.setYearPublished("2011");
-        templist.add(newbook);
-
-        newbook = new Book();
-        newbook.setName("Freakonomics");
-        newbook.setCategory("Kinh tế");
-        newbook.setAuthor("Levitt & Dubner");
-        newbook.setPublisher("Harper");
-        templist.add(newbook);
-
-        newbook = new Book();
-        newbook.setName("Chủ nghĩa khắc kỷ");
-        newbook.setAuthor("William B. Irvine");
-        templist.add(newbook);
-
-        bookRepository.saveAll(templist);
-    }*/
 
     public void addNewBook(ActionEvent actionEvent) throws IOException {
         Dialog<ButtonType> dialog = new Dialog<>();
@@ -194,28 +145,6 @@ public class MainController implements Initializable {
         populateTableViewBooks();
     }
 
-    public void editBook(ActionEvent actionEvent) {
-//        ObservableList itemsDelete = tableViewBooks.getSelectionModel().getSelectedItems();
-//        if (itemsDelete.isEmpty()) {
-//            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//            alert.setTitle("Chỉnh sửa sách...");
-//            alert.setHeaderText("Chọn ít nhất một cuốn sách để chỉnh sửa");
-//            alert.initOwner(mainBorderPane.getScene().getWindow());
-//            alert.showAndWait();
-//            return;
-//        }
-//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-//        alert.setTitle("Xóa sách...");
-//        alert.setResizable(false);
-//        alert.setContentText("Bạn có chắc muốn xóa những sách đã chọn?");
-//        alert.initOwner(mainBorderPane.getScene().getWindow());
-//
-//        Optional<ButtonType> result = alert.showAndWait();
-//        if (result.isPresent() && result.get() == ButtonType.OK) {
-//            bookRepository.deleteAll(itemsDelete);
-//        }
-//        populateTableViewBooks();
-    }
 
     public void deleteBook(ActionEvent actionEvent) {
         ObservableList itemsDelete = tableViewBooks.getSelectionModel().getSelectedItems();
@@ -239,21 +168,6 @@ public class MainController implements Initializable {
         }
         populateTableViewBooks();
     }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-//        studentList = FXCollections.observableArrayList(
-//                new Student(1, "Chau", "chau@gmail.com", 21),
-//                new Student(2, "Chuong", "chuong@gmail.com", 20)
-//        );
-//        idColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("id"));
-//        nameColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("name"));
-//        emailColumn.setCellValueFactory(new PropertyValueFactory<Student, String>("email"));
-//        ageColumn.setCellValueFactory(new PropertyValueFactory<Student, Integer>("age"));
-//        table.setItems(studentList);
-
-    }
-
 
     public void refreshBookList(ActionEvent actionEvent) {
         populateTableViewBooks();
@@ -284,9 +198,7 @@ public class MainController implements Initializable {
             b.setISBN(bookStringCellEditEvent.getNewValue());
         }
 
-        bookRepository.updateBookInfo(b.getId(), b.getName(),
-                b.getCategory(), b.getAuthor(), b.getPublisher(),
-                b.getYearPublished(), b.getISBN());
+        bookRepository.saveAndFlush(b);
 
         populateTableViewBooks();
     }
--- quanlythuvien_javafx_jpa-master/src/main/java/com/quanlythuvien/repository/BookRepository.java
+++ quanlythuvien_javafx_jpa-master/src/main/java/com/quanlythuvien/repository/BookRepository.java
@@ -2,23 +2,19 @@ package com.quanlythuvien.repository;
 
 import com.quanlythuvien.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Integer> {
-    @Transactional
-    @Modifying
-    @Query("UPDATE Book SET name = :name, category = :category," +
-            "author = :author, publisher = :publisher, yearPublished = :yearPublished," +
-            "ISBN = :isbn WHERE id = :bookid")
-    int updateBookInfo(@Param("bookid") int bookid, @Param("name") String name,
-                       @Param("category") String category,
-                       @Param("author") String author,
-                       @Param("publisher") String publisher,
-                       @Param("yearPublished") String yearPublished,
-                       @Param("isbn") String isbn);
+//    @Transactional
+//    @Modifying
+//    @Query("UPDATE Book SET name = :name, category = :category," +
+//            "author = :author, publisher = :publisher, yearPublished = :yearPublished," +
+//            "ISBN = :isbn WHERE id = :bookid")
+//    int updateBookInfo(@Param("bookid") int bookid, @Param("name") String name,
+//                       @Param("category") String category,
+//                       @Param("author") String author,
+//                       @Param("publisher") String publisher,
+//                       @Param("yearPublished") String yearPublished,
+//                       @Param("isbn") String isbn);
 }
